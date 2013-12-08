fb2b24fdb9deb2a78ce76a593f34d6be27da7eaf
Matthew Strickland <matthew@redcrown.co>
2013-12-08 10:05:33 -0800

    fucking capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,16 +6,14 @@ set :copy_via, :scp
 set :branch, "master"
 set :deploy_via, :copy
 
-role :web, "192.227.160.74"
-role :app, "192.227.160.74"
-role :db, "192.227.160.74", :primary => true
-role :db, "192.227.160.74"
-
 set :user, 'frosty'
-server '192.227.160.74', user: 'frosty', roles: %w{web app db} 
 set :deploy_to, "/home/#{fetch(:user)}/Applications/#{fetch(:application)}"
 
-set :log_level, :info
+role :web, "192.227.160.74"
+role :app, "192.227.160.74"
+role :db,  "192.227.160.74", :primary => true
+
+server '192.227.160.74', user: 'frosty', roles: %w{web app db} 
 
 set :ssh_options, {
   user: %w(user),
@@ -25,7 +23,26 @@ set :ssh_options, {
 }
 set :use_sudo, false
 
-#set :linked_files, %w{config/database.yml config/config.yml}
+set :rvm_type, :user
+set :rvm_ruby_version, '2.0.0-p195'
+set :rvm_custom_path, '~/.rvm'
+
+
+set :log_level, :info
+
+set :format, :pretty
+set :pty, true
+
+set :default_env, { rvm_bin_path: "~/.rvm/bin" }
+set :bundle_gemfile, -> { relase_path.join('Gemfile') }
+set :bundle_dir, -> { shared_path.join('bundle') }
+set :bundle_flags, ''
+set :bundle_without, %w{test development}.join(' ')
+set :bundle_binstubs, -> { shared_path.join('bin') }
+set :bundle_roles, :all
+set :bundle_bins, %w(gem rake rails)
+
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp vendor/bundle public/system public/uploads}
 
 SSHKit.config.command_map[:rake]  = "bundle exec rake"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,1 @@
 set :stage, :production
-
-set :rvm_type, :user
-set :rvm_ruby_version, '2.0.0-p195'
-set :rvm_custom_path, '~/.rvm'
